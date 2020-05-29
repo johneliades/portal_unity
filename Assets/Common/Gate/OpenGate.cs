c7ff83f9949bcbb075104b1889a3d2764426ea40
johneliades <36938242+johneliades@users.noreply.github.com>
2020-05-29 05:17:18 +0300

    2 levels complete

---
+++ Assets/Common/Gate/OpenGate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour
+{
+	public GameObject gate;
+	private GameObject sketchup, current;
+
+	private void OnTriggerEnter(Collider collider) {
+		gate.GetComponent<Collider>().enabled = false;
+		sketchup = gate.transform.GetChild(0).gameObject;
+
+		for(int i=1; i<sketchup.transform.childCount; i++) {
+			current = sketchup.transform.GetChild(i).gameObject;
+			Renderer renderer= current.GetComponent<Renderer>();
+			if(renderer!=null)
+				renderer.enabled = false;
+		}
+	}
+
+	private void OnTriggerExit(Collider collider) {
+		gate.GetComponent<Collider>().enabled = true;
+		sketchup = gate.transform.GetChild(0).gameObject;
+
+		for(int i=1; i<sketchup.transform.childCount; i++) {
+			current = sketchup.transform.GetChild(i).gameObject;
+			Renderer renderer= current.GetComponent<Renderer>();
+			if(renderer!=null)
+				renderer.enabled = true;
+		}
+	}
+}
